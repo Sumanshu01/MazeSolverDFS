b9ce19cb83be0eee620457172e365abd11d61e72
Sumanshu Jindal <sumanshujindal01@gmail.com>
2024-12-23 00:00:07 +0530

    Create Sample-Input-Output.cpp

---
+++ Sample-Input-Output.cpp
@@ -0,0 +1,20 @@
+/* SAMPLE INPUT
+Enter the number of rows and columns in the maze: 5 5
+Enter the maze (use 'S' for start, 'E' for end, '.' for paths, and '#' for walls):
+S . . # .
+# # . . .
+. . # # E
+. . . . #
+. # # . #
+*/
+
+/* SAMPLE OUTPUT
+Path found:
+(0, 0) -> (0, 1) -> (1, 1) -> (2, 1) -> (2, 2) -> (2, 3) -> (3, 3) -> (4, 3) -> End
+Solved Maze:
+S * * # .
+# # * * *
+* * # # E
+* * * * #
+* # # * #
+*/
